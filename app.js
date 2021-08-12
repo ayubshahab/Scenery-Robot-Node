6532870696930a06097e00604961b726960769da
Ayub Shahab <ayub.shahab13@gmail.com>
2021-08-12 16:47:38 -0400

    changed port in app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require("express");
 app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + "/public"));
 app.use(express.json());
